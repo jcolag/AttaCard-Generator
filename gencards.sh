9fd8a0bb8b27aa44b9e021fd32d43bd111e04623
John Colagioia <john@colagioia.net>
2015-07-21 06:25:34 -0400

    Clean up generator script

--- gencards.sh
+++ gencards.sh
@@ -1,17 +1,24 @@
-#!/bin/sh
-rm -f a b
+#!/bin/bash
+filea=$(mktemp)
+fileb=$(mktemp)
+copya=$(mktemp)
 for i in Aim Force Evade Defend
 do
-  seq --format=",$i,%g,," 1 14 | tee -a a >> b
+  seq --format=",$i,%g,," 1 14 | tee -a "$filea" >> "$fileb"
 done
-for i in $(cat a)
+cp "$filea" "$copya"
+while IFS= read -r line
 do
-  attr=$(echo $i | cut -f2 -d',')
-  val=$(echo $i | cut -f3 -d',')
-  insert=$(grep . b | grep -vw $attr | sort -R | head -1)
-  iat=$(echo $insert | cut -f2 -d',')
-  ival=$(echo $insert | cut -f3 -d',')
-  sed -i -e "s/$insert//g" b
-  sed -i -e "s/^,$attr,$val,,$/,$attr,$val,$iat,$ival,,/g" a
-done
+  IFS=, read n attr val n n <<< "$line"
+  echo "$n"
+  attr=$(echo "$line" | cut -f2 -d',')
+  val=$(echo "$line" | cut -f3 -d',')
+  insert=$(grep . "$fileb" | grep -vw "$attr" | sort -R | head -1)
+  iat=$(echo "$insert" | cut -f2 -d',')
+  ival=$(echo "$insert" | cut -f3 -d',')
+  sed -i -e "s/$insert//g" "$fileb"
+  sed -i -e "s/^,$attr,$val,,$/,$attr,$val,$iat,$ival,,/g" "$filea"
+done < "$copya"
+cp "$filea" "$1"
+rm -f "$filea" "$fileb" "$copya"
 
