c891f8155b9a395b72f2e9c05d41a1c977cb6998
John Colagioia <john@colagioia.net>
2015-10-29 06:24:41 -0400

    Clean up based on Rubocop advice

--- deck.rb
+++ deck.rb
@@ -1,45 +1,45 @@
+# Encoding: utf-8
 require 'squib'
 require 'csv'
 
-CardData = Hash.new
+CARD_DATA = {}
 
 def bg(why)
-  return CardData[why]['Background']
+  CARD_DATA[why]['Background']
 end
 
 def fg(why)
-  return CardData[why]['Foreground']
+  CARD_DATA[why]['Foreground']
 end
 
 def bg_grad(whyl, whyr)
-  return '(0,0)(825,0) ' + bg(whyl) + '@0.2 ' + bg(whyr) + '@0.8'
+  '(0,0)(825,0) ' + bg(whyl) + '@0.2 ' + bg(whyr) + '@0.8'
 end
 
 def icon(why)
-  return CardData[why]['Icon'] + '.svg'
+  CARD_DATA[why]['Icon'] + '.svg'
 end
 
 CSV.foreach('dynamic.csv', headers: true) do |row|
-  CardData[row['Name']] = row
+  CARD_DATA[row['Name']] = row
 end
 
 Squib::Deck.new(cards: 56, layout: 'layout.yml') do
   deck = csv file: 'cards.csv'
   attrl = deck['AttrL']
   attrr = deck['AttrR']
-  colorl = attrl.collect { |x| fg(x) }
-  colorr = attrr.collect { |x| fg(x) }
-  background color: attrl.map.with_index { |x,i| bg_grad(x, attrr[i]) }
+  colorl = attrl.map { |x| fg(x) }
+  colorr = attrr.map { |x| fg(x) }
+  background color: attrl.map.with_index { |x, i| bg_grad(x, attrr[i]) }
   text str: deck['Title'], layout: :CardTitle, color: colorl
   text str: attrl, layout: :FirstTitle, color: colorl
   text str: deck['ValueL'], layout: :FirstText, color: colorl
   text str: attrr, layout: :SecondTitle, color: colorr
   text str: deck['ValueR'], layout: :SecondText, color: colorr
-  text str: deck['Quote'], layout: :QuoteText, color: colorr #, hint: :red
-  svg file: attrl.collect {|x| icon(x)}, layout: :FirstIcon
-  svg file: attrr.collect {|x| icon(x)}, layout: :SecondIcon
+  text str: deck['Quote'], layout: :QuoteText, color: colorr
+  svg file: attrl.map { |x| icon(x) }, layout: :FirstIcon
+  svg file: attrr.map { |x| icon(x) }, layout: :SecondIcon
   save_png prefix: 'cbat_'
   save_sheet prefix: 'cbat_multisheet_', columns: 4
   hand file: 'hand.png', trim: 0, trim_radius: 50, fill_color: '#0000'
 end
-
